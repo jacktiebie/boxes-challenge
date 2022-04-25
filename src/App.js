f0fe59523d870941c87a0261e16b592ebf1a45db
Jack Tiebie <jacktiebie45@gmail.com>
2022-04-25 14:49:36 +0200

    code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import boxes from './components/boxes'
+import Box from './components/Box'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(props) {
+const [boxess, setBoxess] = useState(boxes)
+
+
+
+  const box = boxess.map(boxess => <Box on={boxess.on} />)
+   return (
+    <main className='grid'>
+        {box}
+    </main>
+)
 }
-
-export default App;
---
+++ src/components/Box.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Box(props) {
+    const styles = {
+        backgroundColor: props.on ? '#222222' : '#cccccc'
+    }
+    return (
+        <div style={styles} className='square'></div>
+    )
+}
