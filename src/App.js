d0d55aa99d7acbc2277518e18dea4624b4c9b4f9
Jack <jacktiebie45@gmail.com>
2022-04-26 13:49:25 +0200

    code

--- src/App.js
+++ src/App.js
@@ -6,8 +6,34 @@ export default function App(props) {
 const [boxess, setBoxess] = useState(boxes)
 
 
+function toggle(id) {
+    setBoxess(prevNum => {
+        const newSquares = []
+        for(let i = 0; i < prevNum.length; i++) {
+            const currentSquare = prevNum[i]
+            if(currentSquare.id === id) {
+                const updatedSquare = {
+                    ...currentSquare,
+                    on: !currentSquare.on
+                }
+                newSquares.push(updatedSquare)
+            } else {
+                newSquares.push(currentSquare)
+            }
+        }
+        return newSquares
+    })
+}
 
-  const box = boxess.map(boxess => <Box on={boxess.on} />)
+
+  const box = boxess.map(boxess =>
+     <Box 
+     key={boxess.id}
+     id={boxess.id}
+     on={boxess.on}
+     handleClick={toggle}
+     />)
+
    return (
     <main className='grid'>
         {box}
--- src/components/Box.js
+++ src/components/Box.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function Box(props) {
+   
+
     const styles = {
         backgroundColor: props.on ? '#222222' : '#cccccc'
     }
+    
+
+
     return (
-        <div style={styles} className='square'></div>
+        <div
+         style={styles} 
+         className='square'
+         onClick={()=>props.handleClick(props.id)}
+         >
+        
+         </div>
     )
 }
